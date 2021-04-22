2404b315d4511bd6b2bc1a2d3a3227cec033b09f
Vlad <krutikov.vladislav0@gmail.com>
2021-04-22 21:52:52 +0500

    some changes

--- client/src/main/java/client/Main.java
+++ client/src/main/java/client/Main.java
@@ -10,11 +10,6 @@ import sun.font.TrueTypeFont;
 import java.awt.*;
 
 public class Main extends Application {
-//    Dimension sSize = Toolkit.getDefaultToolkit ().getScreenSize ();
-//    Rectangle winSize = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
-//
-//    int taskBarHeight = sSize.height - winSize.height;
-
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
@@ -22,7 +17,6 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 1200, 700));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
